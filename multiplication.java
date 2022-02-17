76fc413c778b562ee763aef34919e3d9737bc069
Vishnunayak56 <Vishnunayak56.vn@gmail.com>
2022-02-17 13:47:47 +0530

    multiplication1

---
+++ multiplication.java
@@ -0,0 +1,25 @@
+Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+
+        // This method reads the number provided using keyboard
+        int num1 = scan.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scan.nextInt();
+
+        // Closing Scanner after the use
+        scan.close();
+        
+        // Calculating product of two numbers
+        int product = num1*num2;
+        
+        // Displaying the multiplication result
+        System.out.println("Output: "+product);
+    }
+}
+
+Output:
+
+Enter first number: 15
+Enter second number: 6
+Output: 90
